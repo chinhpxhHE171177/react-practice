7e262a2acc3bfcb3299853243d2bb7384271ae05
chinhpxHE171177 <169163635+chinhpxhHE171177@users.noreply.github.com>
2024-08-19 16:29:46 +0700

    init new project

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import { Row } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Headers from './components/Header';
 import ListUsers from './components/ListUsers';
+import { useState } from 'react';
+import ModalAddNew from './components/ModalAddNew';
+import { sassFalse } from 'sass';
 
 function App() {
+
+  const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
+
+  const handleClose = () => {
+    setIsShowModalAddNew(false);
+  }
   return (
     <div className='app-container'>
       <Headers />
       <Container>
-        <Row>
-          <ListUsers />
-        </Row>
+        <div className='my-3 add-user d-flex justify-content-between'>
+          <h5>List of users: </h5>
+          <button className='btn btn-primary' onClick={() => setIsShowModalAddNew(true)}>Add New User</button>
+        </div>
+        <ListUsers />
       </Container>
+      <ModalAddNew
+        show={isShowModalAddNew}
+        handleClose={handleClose} />
     </div>
   );
 }
